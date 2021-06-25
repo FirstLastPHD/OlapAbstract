495ab2856d9ea533b183ad92940d7b15c8033b87
FirstLastPHD <62648925+FirstLastPHD@users.noreply.github.com>
2021-06-25 12:51:29 +0200

    Delete models/com/zps/models/ibm/db2/query/strings directory

--- models/com/zps/models/ibm/db2/query/strings/IBMDB2SqlQueryStrings.java
+++
@@ -1,9 +0,0 @@
-package com.zps.models.ibm.db2.query.strings;
-
-public interface IBMDB2SqlQueryStrings {
-	
-	public static String zamestQueryValues = "SELECT OSCIS, PRIJMENI, JMENO, TITUL, ODD, NAZODD FROM UP_ZPST2.ZAMEST";
-	
-	public static String msSqlDBValue = "SELECT [timestamp],[Code],[Name],[Commission _],[Global Dimension 1 Code],[Global Dimension 2 Code],[E-Mail],[Phone No_],[Job Title],[Search E-Mail],[E-Mail 2],[Instant Messaging],[Mobile Phone No_] FROM [NAV2009].[dbo].[TAJMAC-ZPS, a_s_$Salesperson_Purchaser] ";;
-
-}
